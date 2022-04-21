e68caa0ed05dfec294d4574561e20bfb7c4d1687
Rishi004 <rishireiko@gmail.com>
2022-04-21 09:08:01 +0530

    Second Commit

--- src/main/java/com/example/jenkinsBasic/JenkinsBasicApplication.java
+++ src/main/java/com/example/jenkinsBasic/JenkinsBasicApplication.java
@@ -2,9 +2,17 @@ package com.example.jenkinsBasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsBasicApplication {
+
+	@GetMapping("/")
+	public String name() {
+		return "Rishi";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsBasicApplication.class, args);
